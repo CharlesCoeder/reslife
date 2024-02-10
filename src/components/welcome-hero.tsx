c8396dd86b2f348119ba525861e809bb47974d13
CharlesCoeder <croederdev@gmail.com>
2024-02-10 11:58:20 -0500

    Remove height from hero

--- src/components/welcome-hero.tsx
+++ src/components/welcome-hero.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function WelcomeHero() {
   return (
-    <div className="flex flex-col items-center text-nyit-blue-900 h-screen mt-8">
+    <div className="flex flex-col items-center text-nyit-blue-900 mt-8">
       <div className="text-center">
         <h1 className="text-5xl font-bold mb-4">Welcome to NYIT ResLife!</h1>
         <p className="text-xl mb-6">Makers, Doers, Inventors, and Innovators</p>
