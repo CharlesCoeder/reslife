6b0b215d848a18df37aff004d5fdb7941a986bf1
CharlesCoeder <croederdev@gmail.com>
2024-02-16 19:50:35 -0500

    Update Image props

--- src/components/welcome-hero.tsx
+++ src/components/welcome-hero.tsx
@@ -8,12 +8,12 @@ export default function WelcomeHero() {
         <h1 className="text-5xl font-bold mb-4">Welcome to NYIT ResLife!</h1>
         <p className="text-xl mb-6">Makers, Doers, Inventors, and Innovators</p>
       </div>
-      <div className="w-40 h-40 relative">
+      <div className="relative">
         <Image
           src="/nyit_bear.png"
           alt="NYIT Bear Logo"
-          layout="fill"
-          objectFit="contain"
+          width={160}
+          height={160}
         />
       </div>
     </div>
